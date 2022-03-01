30e5ca8be81353aec656afd944fc92059149014d
Jeffrey Rose <jtr219@outlook.com>
2022-03-01 13:49:18 -0500

    set up redis

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const session = require("express-session");
+const Redis = require("ioredis");
+const connectRedis = require("connect-redis");
 
 const main = async () => {
   // initialize the app
   const app = express();
+
+  app.set("trust proxy", 1);
 
   // connect mongoose
   await mongoose.connect(process.env.MONGODB_URI);
@@ -14,6 +19,25 @@ const main = async () => {
   } else {
     console.log("Mongoose Not Connected!");
   }
+
+  // connect Redis
+  const RedisStore = connectRedis(session);
+  const redisURL = process.env.REDIS_TLS_URL;
+  let redis;
+  if (redisURL) {
+    redis = new Redis(redisURL, {
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  } else {
+    redis = new Redis(); // auto connect if running on localhost
+  }
+
+  // test redis
+  await redis
+    .ping()
+    .then((pong) => console.log(pong + "! Redis has been connected"));
 
   // set up middleware
   app.use(express.json());
