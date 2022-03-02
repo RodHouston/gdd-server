2faf420d9e97a268027f635907bcf956fb8b1358
Jeffrey Rose <jtr219@outlook.com>
2022-03-02 13:13:37 -0500

    add design model

--- model/Design.js
+++ model/Design.js
@@ -1,6 +1,26 @@
 const mongoose = require("mongoose");
+const Character = require("./Character");
+const Gameplay = require("./Gameplay");
+const Item = require("./Item");
+const Location = require("./Location");
 
-const designSchema = mongoose.Schema({});
+const designSchema = mongoose.Schema({
+  image: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  budget: Number,
+  deadline: Date,
+  monetization: { type: String, required: true },
+  story: { type: String, required: true },
+  creator: { type: mongoose.Schema.Types.ObjectId, required: true },
+  collaborators: [mongoose.Schema.Types.ObjectId],
+  gameplay: [Gameplay],
+  characters: [Character],
+  locations: [Location],
+  items: [Item],
+  gameLoop: [{ type: String, required: true }],
+  stretchGoals: [{ type: String, required: true }],
+});
 
 const Design = mongoose.model("Design", designSchema);
 
