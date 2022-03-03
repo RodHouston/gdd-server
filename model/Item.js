abf8aef5ac63f40511c78ee96b625751a3a92bc9
Jeffrey Rose <jtr219@outlook.com>
2022-03-03 09:08:16 -0500

    make gameplay and item models consistent

--- model/Item.js
+++ model/Item.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const itemSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  image: { type: String, required: true },
 });
 
 module.exports = itemSchema;
