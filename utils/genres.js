a4cb48ea86d114130da2821e7b4cc7fd9cd46440
Jeffrey Rose <jtr219@outlook.com>
2022-03-03 15:56:33 -0500

    add genres

---
+++ utils/genres.js
@@ -0,0 +1,17 @@
+const GENRE = [
+  "ACTION",
+  "ADVENTURE",
+  "FIGHTING",
+  "MISC",
+  "PLATFORM",
+  "PUZZLE",
+  "RACING",
+  "ROLEPLAYING",
+  "SHOOTER",
+  "SIMULATION",
+  "SPORTS",
+  "STRATEGY",
+  "OTHER",
+];
+
+module.exports = GENRE;
